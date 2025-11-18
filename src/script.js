bba052b47548e21bcabe7b5db722dd187944aab0
smabari <mdazizulbarishafin@gmail.com>
2025-11-18 19:23:43 +0600

    position experimenting

--- src/script.js
+++ src/script.js
@@ -72,9 +72,9 @@ const tick = () => {
   //   mesh.rotation.y = elapsedTime;
 
   //   Update camera
-  camera.position.x = cursor.x * 10;
-  camera.position.y = cursor.y * 10;
-  camera.lookAt(mesh.position)
+  camera.position.x = Math.sin(cursor.x * 10) * 3;
+  camera.position.y = Math.cos(cursor.x * 10) * 3;
+  camera.lookAt(mesh.position);
 
   // Render
   renderer.render(scene, camera);
